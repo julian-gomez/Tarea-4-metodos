d1d68310172b3c34aa04d2b435c24b74dfd75709
julian-gomez <jd.calderon@uniandes.edu.co>
2018-11-11 16:49:15 -0500

    ODE

--- ODE.cpp
+++ ODE.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 //Se resolvera usando el metodo de Runge Kutta 4 orden
-double h = 0.001;
+double h = 0.0001;
 //Definimos las constantes:
 double c = 0.2;
 double g = 10.0;
@@ -16,16 +16,16 @@ double x0 = 0.0;
 double y_0 = 0.0;
 double t0 = 0.0;
 double v0 = 300.0;
-double func_prime1X(double t, double x_1, double x_2,double theta);
-double func_prime2X(double t, double x_1, double x_2,double theta);
-double func_prime1Y(double t, double y_1, double y_2,double theta);
-double func_prime2Y(double t, double y_1, double y_2,double theta);
+double func_prime1X(double t, double x_1, double x_2,double y_2);
+double func_prime2X(double t, double x_1, double x_2,double y_2);
+double func_prime1Y(double t, double y_1, double y_2,double x_2);
+double func_prime2Y(double t, double y_1, double y_2,double x_2);
 string nombres [] = {"datos10.txt", "datos20.txt", "datos30.txt", "datos40.txt", "datos50.txt", "datos60.txt", "datos70.txt", "datos45.txt"};
 
 int main()
 {
 	
-	//Resolviendo para x
+	//Resolviendo
 	int i,j,k,l;
 	double valoresX[8];
 	double t = t0;
@@ -47,66 +47,36 @@ int main()
 		posX = x0;
 		velY = v0*sin(angulo[k]*M_PI/180.0);
 		velX = v0*cos(angulo[k]*M_PI/180.0);
+		archivo_salida << t << " " << posX << " " << posY << " " << velX << " " << velY << endl;
 		while(req == 1)
 		{
 			numPuntos += 1;
-			ky1_1 = func_prime1Y(t,posY,velY,angulo[k]);
-			ky1_2 = func_prime2Y(t,posY,velY,angulo[k]);
+			ky1_1 = func_prime1Y(t,posY,velY,velX);
+			ky1_2 = func_prime2Y(t,posY,velY,velX);
 		
-			ky2_1 = func_prime1Y(t + h/2.0, posY + h/2.0*ky1_1, velY + h/2.0*ky1_2,angulo[k]);
-			ky2_2 = func_prime2Y(t + h/2.0, posY + h/2.0*ky1_1, velY + h/2.0*ky1_2,angulo[k]);
+			ky2_1 = func_prime1Y(t + h/2.0, posY + h/2.0*ky1_1, velY + h/2.0*ky1_2,velX);
+			ky2_2 = func_prime2Y(t + h/2.0, posY + h/2.0*ky1_1, velY + h/2.0*ky1_2,velX);
 
-			ky3_1 = func_prime1Y(t + h/2.0, posY + h/2.0*ky2_1, velY + h/2.0*ky2_2,angulo[k]);
-			ky3_2 = func_prime2Y(t + h/2.0, posY + h/2.0*ky2_1, velY + h/2.0*ky2_2,angulo[k]);
+			ky3_1 = func_prime1Y(t + h/2.0, posY + h/2.0*ky2_1, velY + h/2.0*ky2_2,velX);
+			ky3_2 = func_prime2Y(t + h/2.0, posY + h/2.0*ky2_1, velY + h/2.0*ky2_2,velX);
 
-			ky4_1 = func_prime1Y(t + h, posY + h*ky3_1, velY + h*ky3_2,angulo[k]);
-			ky4_2 = func_prime2Y(t + h, posY + h*ky3_1, velY + h*ky3_2,angulo[k]);
+			ky4_1 = func_prime1Y(t + h, posY + h*ky3_1, velY + h*ky3_2,velX);
+			ky4_2 = func_prime2Y(t + h, posY + h*ky3_1, velY + h*ky3_2,velX);
+
+			kx1_1 = func_prime1X(t,posX,velX,velY);
+			kx1_2 = func_prime2X(t,posX,velX,velY);
+			
+			kx2_1 = func_prime1X(t + h/2.0,posX + h/2.0*kx1_1, velX + h/2.0*kx1_2,velY);
+			kx2_2 = func_prime2X(t + h/2.0,posX + h/2.0*kx1_1, velX + h/2.0*kx1_2,velY);
+
+			kx3_1 = func_prime1X(t + h/2.0,posX + h/2.0*kx2_1, velX + h/2.0*kx2_2,velY);
+			kx3_2 = func_prime2X(t + h/2.0,posX + h/2.0*kx2_1, velX + h/2.0*kx2_2,velY);
+
+			kx4_1 = func_prime1X(t + h,posX + h*kx3_1, velX + h*kx3_2,velY);
+			kx4_2 = func_prime2X(t + h,posX + h*kx3_1, velX + h*kx3_2,velY);			
 
 			promedioy1 = (ky1_1 + 2.0*ky2_1 + 2.0*ky3_1 + ky4_1)/6.0;
 			promedioy2 = (ky1_2 + 2.0*ky2_2 + 2.0*ky3_2 + ky4_2)/6.0;
-		
-			t = t + h;
-			posY = posY + h*promedioy1;
-			velY = velY + h*promedioy2;
-			if(posY <= 0.0)
-			{
-				req = 0;
-			}
-		}
-	
-		//Ya contamos con el numero de puntos que hay hasta que el proyectil alcanza el suelo de nuevo. Ahora, haremos el mismo proceso, pero imprimiendo los 5 datos: tiempo, posicionX, posicionY, velocidadX, velocidadY
-		t = t0;
-		posY = y_0;
-		velY = v0*sin(angulo[k]*M_PI/180.0);
-		archivo_salida << t << " " << posX << " " << posY << " " << velX << " " << velY << endl;
-		for(i = 0; i < numPuntos - 1; i++)
-		{
-			ky1_1 = func_prime1Y(t,posY,velY,angulo[k]);
-			ky1_2 = func_prime2Y(t,posY,velY,angulo[k]);
-		
-			ky2_1 = func_prime1Y(t + h/2.0, posY + h/2.0*ky1_1, velY + h/2.0*ky1_2,angulo[k]);
-			ky2_2 = func_prime2Y(t + h/2.0, posY + h/2.0*ky1_1, velY + h/2.0*ky1_2,angulo[k]);
-
-			ky3_1 = func_prime1Y(t + h/2.0, posY + h/2.0*ky2_1, velY + h/2.0*ky2_2,angulo[k]);
-			ky3_2 = func_prime2Y(t + h/2.0, posY + h/2.0*ky2_1, velY + h/2.0*ky2_2,angulo[k]);
-
-			ky4_1 = func_prime1Y(t + h, posY + h*ky3_1, velY + h*ky3_2,angulo[k]);
-			ky4_2 = func_prime2Y(t + h, posY + h*ky3_1, velY + h*ky3_2,angulo[k]);
-
-			promedioy1 = (ky1_1 + 2.0*ky2_1 + 2.0*ky3_1 + ky4_1)/6.0;
-			promedioy2 = (ky1_2 + 2.0*ky2_2 + 2.0*ky3_2 + ky4_2)/6.0;
-
-			kx1_1 = func_prime1X(t,posX,velX,angulo[k]);
-			kx1_2 = func_prime2X(t,posX,velX,angulo[k]);
-		
-			kx2_1 = func_prime1X(t + h/2.0, posX + h/2.0*kx1_1, velX + h/2.0*kx1_2,angulo[k]);
-			kx2_2 = func_prime2X(t + h/2.0, posX + h/2.0*kx1_1, velX + h/2.0*kx1_2,angulo[k]);
-
-			kx3_1 = func_prime1X(t + h/2.0, posX + h/2.0*kx2_1, velX + h/2.0*kx2_2,angulo[k]);
-			kx3_2 = func_prime2X(t + h/2.0, posX + h/2.0*kx2_1, velX + h/2.0*kx2_2,angulo[k]);
-	
-			kx4_1 = func_prime1X(t + h, posX + h*kx3_1, velX + h*kx3_2,angulo[k]);
-			kx4_2 = func_prime2X(t + h, posX + h*kx3_1, velX + h*kx3_2,angulo[k]);
 
 			promediox1 = (kx1_1 + 2.0*kx2_1 + 2.0*kx3_1 + kx4_1)/6.0;
 			promediox2 = (kx1_2 + 2.0*kx2_2 + 2.0*kx3_2 + kx4_2)/6.0;
@@ -115,7 +85,56 @@ int main()
 			posY = posY + h*promedioy1;
 			velY = velY + h*promedioy2;
 			posX = posX + h*promediox1;
-			velX = velX + h*promediox2;
+			velY = velY + h*promediox2;
+			if(posY <= 0.0)
+			{
+				req = 0;
+			}
+		}
+	
+		//Ya contamos con el numero de puntos que hay hasta que el proyectil alcanza el suelo de nuevo. Ahora, haremos el mismo proceso, pero imprimiendo los 5 datos: tiempo, posicionX, posicionY, velocidadX, velocidadY
+		t = t0;
+	 	posY = y_0;
+		posX = x0;
+		velY = v0*sin(angulo[k]*M_PI/180.0);
+		velX = v0*cos(angulo[k]*M_PI/180.0);
+		for(i = 0; i < numPuntos - 1; i++)
+		{
+			ky1_1 = func_prime1Y(t,posY,velY,velX);
+			ky1_2 = func_prime2Y(t,posY,velY,velX);
+		
+			ky2_1 = func_prime1Y(t + h/2.0, posY + h/2.0*ky1_1, velY + h/2.0*ky1_2,velX);
+			ky2_2 = func_prime2Y(t + h/2.0, posY + h/2.0*ky1_1, velY + h/2.0*ky1_2,velX);
+
+			ky3_1 = func_prime1Y(t + h/2.0, posY + h/2.0*ky2_1, velY + h/2.0*ky2_2,velX);
+			ky3_2 = func_prime2Y(t + h/2.0, posY + h/2.0*ky2_1, velY + h/2.0*ky2_2,velX);
+
+			ky4_1 = func_prime1Y(t + h, posY + h*ky3_1, velY + h*ky3_2,velX);
+			ky4_2 = func_prime2Y(t + h, posY + h*ky3_1, velY + h*ky3_2,velX);
+
+			kx1_1 = func_prime1X(t,posX,velX,velY);
+			kx1_2 = func_prime2X(t,posX,velX,velY);
+			
+			kx2_1 = func_prime1X(t + h/2.0,posX + h/2.0*kx1_1, velX + h/2.0*kx1_2,velY);
+			kx2_2 = func_prime2X(t + h/2.0,posX + h/2.0*kx1_1, velX + h/2.0*kx1_2,velY);
+
+			kx3_1 = func_prime1X(t + h/2.0,posX + h/2.0*kx2_1, velX + h/2.0*kx2_2,velY);
+			kx3_2 = func_prime2X(t + h/2.0,posX + h/2.0*kx2_1, velX + h/2.0*kx2_2,velY);
+
+			kx4_1 = func_prime1X(t + h,posX + h*kx3_1, velX + h*kx3_2,velY);
+			kx4_2 = func_prime2X(t + h,posX + h*kx3_1, velX + h*kx3_2,velY);			
+
+			promedioy1 = (ky1_1 + 2.0*ky2_1 + 2.0*ky3_1 + ky4_1)/6.0;
+			promedioy2 = (ky1_2 + 2.0*ky2_2 + 2.0*ky3_2 + ky4_2)/6.0;
+
+			promediox1 = (kx1_1 + 2.0*kx2_1 + 2.0*kx3_1 + kx4_1)/6.0;
+			promediox2 = (kx1_2 + 2.0*kx2_2 + 2.0*kx3_2 + kx4_2)/6.0;
+		
+			t = t + h;
+			posY = posY + h*promedioy1;
+			velY = velY + h*promedioy2;
+			posX = posX + h*promediox1;
+			velY = velY + h*promediox2;
 			archivo_salida << t << " " << posX << " " << posY << " " << velX << " " << velY << endl;
 			if ((i == numPuntos - 2))
 			{
@@ -143,23 +162,23 @@ int main()
 	return 0;
 }
 
-double func_prime1X(double t, double x_1, double x_2, double theta)
+double func_prime1X(double t, double x_1, double x_2, double y_2)
 {
 	return x_2;
 }
 
-double func_prime2X(double t, double x_1, double x_2, double theta)
+double func_prime2X(double t, double x_1, double x_2, double y_2)
 {
-	return -c/(m*cos(theta*M_PI/180.0))*abs(x_2)*x_2;
+	return -c/m*pow((pow(x_2,2) + pow(y_2,2)),1.0/2.0)*x_2;
 }
 
-double func_prime1Y(double t, double y_1, double y_2, double theta)
+double func_prime1Y(double t, double y_1, double y_2, double x_2)
 {
 	return y_2;
 }
-double func_prime2Y(double t, double y_1, double y_2, double theta)
+double func_prime2Y(double t, double y_1, double y_2, double x_2)
 {
-	return -g-c/(m*sin(theta*M_PI/180.0))*abs(y_2)*y_2;
+	return -g-c/m*pow((pow(x_2,2) + pow(y_2,2)),1.0/2.0)*y_2;
 }
 
 
--- Plots_hw4.py
+++ Plots_hw4.py
@@ -4,7 +4,12 @@ import matplotlib.pyplot as plt
 plt.figure()
 for i in range(6):
 	datos = np.genfromtxt("datos" + str((i+2)*10) + ".txt", delimiter = " ", usecols = (1,2))
-	plt.plot(datos[:,0], datos[:,1])
+	plt.plot(datos[:,0], datos[:,1], label = "Angulo " + str((i + 1)*10))
 
+plt.legend()
+plt.show()
 
+datos45 = np.genfromtxt("datos45.txt", delimiter = " ", usecols = (1,2))
+plt.plot(datos45[:,0], datos45[:,1], label = "Angulo 45")
+plt.legend()
 plt.show()
