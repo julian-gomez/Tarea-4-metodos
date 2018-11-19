7ac5eddabf72eb56b00f0f19c47a2c823de16df4
julian-gomez <42391147+julian-gomez@users.noreply.github.com>
2018-11-19 12:48:05 -0500

    Add files via upload

--- hw4.mk
+++ hw4.mk
@@ -1,5 +1,5 @@
-resultados_hw4.pdf:PlotsAngulos.pdf Plots45.pdf CondicionesIniciales.pdf FijoFinal.pdf FijoIntermedio1.pdf FijoIntermedio2.pdf PromedioFijo.pdf LibreFinal.pdf LibreIntermedio1.pdf LibreIntermedio2.pdf LibrePromedio.pdf PeriodicaFinal.pdf PeriodicaIntermedia1.pdf PeriodicaIntermedia2.pdf PeriodicaPromedio.pdf resultados_hw4.tex
-	pdflatex resultados_hw4.tex
+resultados_hw4.pdf:PlotsAngulos.pdf Plots45.pdf CondicionesIniciales.pdf FijoFinal.pdf FijoIntermedio1.pdf FijoIntermedio2.pdf PromedioFijo.pdf LibreFinal.pdf LibreIntermedio1.pdf LibreIntermedio2.pdf LibrePromedio.pdf PeriodicaFinal.pdf PeriodicaIntermedia1.pdf PeriodicaIntermedia2.pdf PeriodicaPromedio.pdf Resultados_hw4.tex
+	pdflatex Resultados_hw4.tex
 Plots45.pdf:datosProyectil.txt Plots_hw4.py
 	python Plots_hw4.py
 PlotsAngulos.pdf:datosProyectil.txt Plots_hw4.py
